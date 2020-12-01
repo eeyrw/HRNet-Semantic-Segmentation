74970df2592323c207d1c41373fc0dea494d150d
yuan <ayrw@163.com>
2020-12-01 21:49:36 +0800

    M

---
+++ data/dataloader.py
@@ -0,0 +1,199 @@
+import torch
+import os
+import numpy as np
+
+import torchvision.transforms as transforms
+import mytransforms as mytransforms
+from constant import tusimple_row_anchor, culane_row_anchor
+from dataset import LaneClsDataset, LaneTestDataset, LaneGenPseudoDataset
+from datasetUtils import get_partial_dataset, split_dataset
+
+
+def get_train_loader(batch_size, data_root, griding_num, dataset, use_aux,
+                     distributed, num_lanes, proportion=1, split=False, split_proportion=0.5, load_name=False, pin_memory=False, num_workers=4):
+    target_transform = transforms.Compose([
+        mytransforms.FreeScaleMask((288, 800)),
+        mytransforms.MaskToTensor(),
+    ])
+    segment_transform = transforms.Compose([
+        mytransforms.FreeScaleMask((36, 100)),
+        mytransforms.MaskToTensor(),
+    ])
+    img_transform = transforms.Compose([
+        transforms.Resize((288, 800)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    simu_transform = mytransforms.Compose2([
+        mytransforms.RandomRotate(6),
+        mytransforms.RandomUDoffsetLABEL(100),
+        mytransforms.RandomLROffsetLABEL(200)
+    ])
+    if dataset == 'CULane':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(
+                                           data_root, 'list/train_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=simu_transform,
+                                       segment_transform=segment_transform,
+                                       row_anchor=culane_row_anchor,
+                                       griding_num=griding_num, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 18
+    elif dataset == 'CULane-pseudo':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(
+                                           data_root, 'train_pseudo_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=simu_transform,
+                                       griding_num=griding_num,
+                                       row_anchor=culane_row_anchor,
+                                       segment_transform=segment_transform, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 18
+
+    elif dataset == 'CULane-no-aug':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(
+                                           data_root, 'list/train_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=None,
+                                       griding_num=griding_num,
+                                       row_anchor=culane_row_anchor,
+                                       segment_transform=segment_transform, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 18
+    elif dataset == 'Tusimple':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(data_root, 'train_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=simu_transform,
+                                       griding_num=griding_num,
+                                       row_anchor=tusimple_row_anchor,
+                                       segment_transform=segment_transform, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 56
+    elif dataset == 'Tusimple-pseudo':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(
+                                           data_root, 'train_pseudo_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=simu_transform,
+                                       griding_num=griding_num,
+                                       row_anchor=tusimple_row_anchor,
+                                       segment_transform=segment_transform, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 56
+
+    elif dataset == 'Tusimple-no-aug':
+        train_dataset = LaneClsDataset(data_root,
+                                       os.path.join(
+                                           data_root, 'train_gt.txt'),
+                                       img_transform=img_transform, target_transform=target_transform,
+                                       simu_transform=None,
+                                       griding_num=griding_num,
+                                       row_anchor=tusimple_row_anchor,
+                                       segment_transform=segment_transform, use_aux=use_aux, num_lanes=num_lanes, load_name=load_name)
+        cls_num_per_lane = 56
+    else:
+        raise NotImplementedError
+
+    train_dataset = get_partial_dataset(train_dataset, proportion)
+    if distributed:
+        sampler = torch.utils.data.distributed.DistributedSampler(
+            train_dataset)
+    else:
+        sampler = torch.utils.data.RandomSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=sampler, num_workers=4)
+
+    if split:
+        train_datasets = split_dataset(train_dataset, split_proportion)
+        if distributed:
+            samplers = [torch.utils.data.distributed.DistributedSampler(
+                ds) for ds in train_datasets]
+        else:
+            samplers = [torch.utils.data.RandomSampler(
+                ds) for ds in train_datasets]
+        train_loader = [torch.utils.data.DataLoader(ds, batch_size=batch_size, sampler=sampler, num_workers=num_workers, pin_memory=pin_memory)
+                        for ds, sampler in zip(train_datasets, samplers)]
+
+    return train_loader, cls_num_per_lane
+
+
+def get_test_loader(batch_size, data_root, dataset, distributed, proportion=1, num_workers=4):
+    img_transforms = transforms.Compose([
+        transforms.Resize((288, 800)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    if dataset == 'CULane':
+        test_dataset = LaneTestDataset(data_root, os.path.join(
+            data_root, 'list/test.txt'), img_transform=img_transforms)
+        cls_num_per_lane = 18
+    elif dataset == 'Tusimple':
+        test_dataset = LaneTestDataset(data_root, os.path.join(
+            data_root, 'test.txt'), img_transform=img_transforms)
+        cls_num_per_lane = 56
+
+    test_dataset = get_partial_dataset(test_dataset, proportion)
+
+    if distributed:
+        sampler = SeqDistributedSampler(test_dataset, shuffle=False)
+    else:
+        sampler = torch.utils.data.SequentialSampler(test_dataset)
+    loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size, sampler=sampler, num_workers=num_workers)
+    return loader
+
+
+def get_gen_pseudo_loader(batch_size, data_root, dataset, distributed, num_workers=4):
+    img_transforms = transforms.Compose([
+        transforms.Resize((288, 800)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    if dataset == 'CULane':
+        test_dataset = LaneGenPseudoDataset(data_root, os.path.join(
+            data_root, 'list/train_pseudo_gt.txt'), img_transform=img_transforms)
+        cls_num_per_lane = 18
+    elif dataset == 'Tusimple':
+        test_dataset = LaneGenPseudoDataset(data_root, os.path.join(
+            data_root, 'train_pseudo_gt.txt'), img_transform=img_transforms)
+        cls_num_per_lane = 56
+
+    if distributed:
+        sampler = SeqDistributedSampler(test_dataset, shuffle=False)
+    else:
+        sampler = torch.utils.data.SequentialSampler(test_dataset)
+    loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size, sampler=sampler, num_workers=num_workers)
+    return loader
+
+
+class SeqDistributedSampler(torch.utils.data.distributed.DistributedSampler):
+    '''
+    Change the behavior of DistributedSampler to sequential distributed sampling.
+    The sequential sampling helps the stability of multi-thread testing, which needs multi-thread file io.
+    Without sequentially sampling, the file io on thread may interfere other threads.
+    '''
+
+    def __init__(self, dataset, num_replicas=None, rank=None, shuffle=False):
+        super().__init__(dataset, num_replicas, rank, shuffle)
+
+    def __iter__(self):
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+        if self.shuffle:
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = list(range(len(self.dataset)))
+
+        # add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        num_per_rank = int(self.total_size // self.num_replicas)
+
+        # sequential sampling
+        indices = indices[num_per_rank *
+                          self.rank: num_per_rank * (self.rank + 1)]
+
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
---
+++ tools/gen_embedding.py
@@ -0,0 +1,210 @@
+import os
+import time
+import shutil
+import torch
+import torchvision
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import cv2
+import numpy as np
+import torchvision.transforms as transforms
+from PIL import Image
+from collections import namedtuple
+import scipy.io
+from tqdm import tqdm
+import argparse
+
+import _init_paths
+
+from extraDs.cityscapes import Cityscapes
+import models
+from config import config
+from config import update_config
+import torchvision.transforms as transforms
+import mytransforms as mytransforms
+from constant import tusimple_row_anchor, culane_row_anchor
+from dataset import LaneClsDataset, LaneTestDataset, LaneGenPseudoDataset
+from datasetUtils import get_partial_dataset, split_dataset
+
+input_mean = [0.485, 0.456, 0.406]
+input_std = [0.229, 0.224, 0.225]
+
+def getTusimpleLoader():
+    target_transform = transforms.Compose([
+        mytransforms.Scale((512, 1024)),
+        mytransforms.MaskToTensor(),
+    ])
+    segment_transform = transforms.Compose([
+        mytransforms.FreeScaleMask((36, 100)),
+        mytransforms.MaskToTensor(),
+    ])
+    img_transform = transforms.Compose([
+        transforms.CenterCrop((512, 1024)),
+        transforms.ToTensor(),
+        transforms.Normalize(input_mean, input_std),
+    ])
+
+    train_dataset = LaneClsDataset('E:/Tusimple',
+                                   os.path.join(
+                                       'E:/Tusimple', 'train_gt.txt'),
+                                   img_transform=img_transform, target_transform=target_transform,
+                                   simu_transform=None,
+                                   griding_num=100,
+                                   row_anchor=tusimple_row_anchor,
+                                   segment_transform=segment_transform, use_aux=False, num_lanes=4, load_name=True)
+
+    sampler = torch.utils.data.SequentialSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1, sampler=sampler, num_workers=4)
+
+    return train_loader
+
+
+def getCulaneLoader():
+    target_transform = transforms.Compose([
+        mytransforms.Scale((512, 1024)),
+        mytransforms.MaskToTensor(),
+    ])
+    segment_transform = transforms.Compose([
+        mytransforms.FreeScaleMask((36, 100)),
+        mytransforms.MaskToTensor(),
+    ])
+    img_transform = transforms.Compose([
+        transforms.CenterCrop((512, 1024)),
+        transforms.ToTensor(),
+        transforms.Normalize(input_mean, input_std),
+    ])
+
+    train_dataset = LaneClsDataset('E:/CUlane',
+                                   os.path.join(
+                                       'E:/CUlane', 'list/train_gt.txt'),
+                                   img_transform=img_transform, target_transform=target_transform,
+                                   simu_transform=None,
+                                   griding_num=200,
+                                   row_anchor=culane_row_anchor,
+                                   segment_transform=segment_transform, use_aux=False, num_lanes=4, load_name=True)
+
+    sampler = torch.utils.data.SequentialSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=8, sampler=sampler, num_workers=4)
+
+    return train_loader    
+
+def resizeAndCropToTargetSize(img, width, height):
+    rawW, rawH = img.size
+    rawAspectRatio = rawW/rawH
+    wantedAspectRatio = width/height
+    if rawAspectRatio > wantedAspectRatio:
+        scaleFactor = height/rawH
+        widthBeforeCrop = int(rawW*scaleFactor)
+        return img.resize((widthBeforeCrop, height), Image.BILINEAR). \
+            crop(((widthBeforeCrop-width)//2, 0,
+                  (widthBeforeCrop-width)//2+width, height))
+    else:
+        scaleFactor = width/rawW
+        heightBeforeCrop = int(rawH*scaleFactor)
+        return img.resize((width, heightBeforeCrop), Image.BILINEAR). \
+            crop((0, (heightBeforeCrop-height)//2, width,
+                  (heightBeforeCrop-height)//2+height))
+
+def normalizeImage(imageTensor):
+    maxVal = torch.max(imageTensor)
+    minVal = torch.min(imageTensor)
+    imageNormalized = (imageTensor-minVal)/(maxVal-minVal)
+    return (imageNormalized*255).byte().cpu().numpy()
+
+def tensorToCvBgr(image):
+    # Step 1. chw to hwc Step 2. RGB to BGR
+    img_bgr = normalizeImage(image).transpose((1, 2, 0))[..., ::-1]
+    return img_bgr
+
+def main():
+    device = 'cuda'
+
+    parser = argparse.ArgumentParser(description='Train segmentation network')
+
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args(
+        args=["--cfg", "experiments\cityscapes\seg_hrnet_ocr_w48_demo.yaml"])
+    update_config(config, args)
+
+    # build model
+    if torch.__version__.startswith('1'):
+        module = eval('models.'+config.MODEL.NAME)
+        module.BatchNorm2d_class = module.BatchNorm2d = torch.nn.BatchNorm2d
+    model = eval('models.'+config.MODEL.NAME +
+                 '.get_seg_model')(config)
+
+    model.to(device)
+
+
+    weightFile = '../weight/hrnet_ocr_cs_trainval_8227_torch11.pth'
+    # weightFile = 'pspnet_46_6.pth.tar'
+
+    if os.path.isfile(weightFile):
+        # print(("=> loading checkpoint '{}'".format('pspnet_46_6.pth.tar')))
+        checkpoint = torch.load(weightFile)
+        # torch.nn.Module.load_state_dict(model, checkpoint['state_dict'])
+        compatible_state_dict = {}
+        for k, v in checkpoint.items():
+            if 'model.' in k:
+                compatible_state_dict[k[6:]] = v
+            elif 'loss.' in k:
+                pass
+            else:
+                compatible_state_dict[k] = v
+
+        model.load_state_dict(compatible_state_dict, strict=True)
+        model.eval()
+
+    cudnn.benchmark = True
+    cudnn.fastest = True
+
+
+    train_loader = getCulaneLoader()
+    for imageFile in tqdm(train_loader):
+        imgs = imageFile[0].cuda()
+
+        with torch.no_grad():
+            segOutput = model(imgs)[1]
+
+        # imageBgrCV = cv2.cvtColor(np.asarray(resizeImage), cv2.COLOR_RGB2BGR)
+        # imageBgrCV = np.zeros((512,1024,3),dtype=np.uint8)
+        imageBgrCV = tensorToCvBgr(imgs[0])
+        segOutput = segOutput[0]
+        # segOutput: [class,h,w]
+        # t = torch.sigmoid(segOutput)
+        t = torch.argmax(segOutput, dim=0)
+        segOutput = t.byte().cpu().numpy()
+        # segOutput: [1,1,h,w]
+
+        # colorMapMat = np.array([lb.color for lb in labels],dtype=np.uint8)[...,::-1] # RGB to BGR
+        segImage = Cityscapes.decode_target(
+            segOutput).astype(np.uint8)[..., ::-1]
+        segImage = cv2.resize(segImage, (1024, 512),
+                              interpolation=cv2.INTER_NEAREST)
+        # segImage = colorMapMat[segOutput]
+        imageBgrCV = cv2.addWeighted(imageBgrCV, 0.5, segImage, 0.5, 0)
+        # imageBgrCV = segImage
+        # imageBgrCV = cv2.resize(imageBgrCV,(3384//4,2710//4))
+
+        cv2.imshow('L', imageBgrCV)
+        # The following frees up resources and closes all windows
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
+    cv2.destroyWindow('L')
+    return
+
+
+if __name__ == '__main__':
+    main()
